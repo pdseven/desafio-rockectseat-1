2383395f9d0fb968512e0930ba442ffda394202d
Diego Neves <pdcassiano@gmail.com>
2020-07-27 17:25:35 -0300

    projeto completo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require('uuidv4')
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,78 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repositorie)
+
+  return response.json(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs} = request.body
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie nto found'})
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes
+  }
+
+  repositories[repositorieIndex] = repositorie
+
+  return response.json(repositorie)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie nto found'})
+  }
+
+  repositories.splice(repositorieIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie nto found'})
+  }
+
+  const repositorieOld = repositories[repositorieIndex]
+
+  repositorieNew = {
+    id: repositorieOld.id,
+    title: repositorieOld.title,
+    url: repositorieOld.url,
+    techs: repositorieOld.techs,
+    likes: ++repositorieOld.likes
+  }
+
+  repositories[repositorieIndex] = repositorieNew
+
+  return response.json(repositorieNew)
 });
 
 module.exports = app;
